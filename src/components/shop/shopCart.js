d14307cc17cf121d48568b7df1146e1ec4ce88c3
mjg1123 <53385037+mjg1123@users.noreply.github.com>
2019-12-10 12:01:41 -0800

    built shop-cart-content-footer component and styled the checkout button

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faIcon } from '@fortawesome/free-solid-svg-icons';
 
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
-            <FontAwesomeIcon className={`${icon}`} icon={icon} />        
+            <i className={icon}/>        
         </div>
     )
 }
 
-function CartContent({className}) {
+function CartContent({className, products}) {
+    let count = products.length;
+    let prodcutsJSX = products.map(product => <h1 key={product} >{product}</h1>)
     return (
         <div className={`${className} cart-content`}>
+            <div className='cart-content__title'>
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
+                {prodcutsJSX}
+            </div>
+            <div className='cart-content__footer'>
 
+            </div>
+            <CartFooter className='cart-content__footer' prodcuts={products} />
+        </div>
+    )
+}
+
+function CartFooter ({ className, prodcuts}) {
+    const price = 7.96;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className='cart-footer__checkout'>
+                Checkout
+            </a>
+            <div className='cart-footer__subtotal'>
+                Subtotal
+            </div>
+            <div className='cart-footer__price'>
+                ${price}
+            </div>
         </div>
     )
 }
@@ -23,8 +49,8 @@ class ShopCart extends Component {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
-                <CartButton className='shop-cart__toggle' icon='faTimes'/>
-                <CartContent className='shop-cart__content'/>
+                <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
+                <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
             </div>
         )
     }
