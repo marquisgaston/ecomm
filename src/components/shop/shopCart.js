7a0a0dc3999e87bb7859449651e46b419aac977c
mjg1123 <53385037+mjg1123@users.noreply.github.com>
2019-12-11 02:01:56 -0500

    setup cartProduct medel to fetch user cart

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import CartProduct from './cartProduct';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
@@ -11,7 +14,7 @@ function CartButton({className, icon}) {
 
 function CartContent({className, products}) {
     let count = products.length;
-    let prodcutsJSX = products.map(product => <CartProduct key={product}  />)
+    let prodcutsJSX = products.map(product => <CartProduct key={product._id}  />)
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
@@ -46,15 +49,28 @@ function CartFooter ({ className, prodcuts}) {
 }
 
 class ShopCart extends Component {
+    componentDidMount () {
+        this.props.fetchCartProducts();
+    }
     render() {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554,243, 3434, 4554,243, 3434, 4554,243, 3434, 4554]}/>
+                <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
             </div>
         )
     }
 }
+
+function mapStateToProps (state) {
+    const { cartProducts } = state.user;
+    console.log(cartProducts);
+    return {
+        cartProducts
+    }
+}
+
+ShopCart = connect(mapStateToProps, actions)(ShopCart);
 
 export default ShopCart;
