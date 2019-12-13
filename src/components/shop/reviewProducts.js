0b3196209f0a1ffdaade3015991a65d031673d6e
mjg1123 <53385037+mjg1123@users.noreply.github.com>
2019-12-13 01:49:14 -0500

    built order review products component

---
+++ src/components/shop/reviewProducts.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class reviewProducts extends Component {
+    render() {
+        const { className } = this.props;
+        return (
+            <div className={`${className} review-products`}>
+                {
+                    this.props.cartProducts.map(cartProduct => {
+                        return <h1 key={cartProduct._id}>{cartProduct.product.title}</h1>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps (state) {
+    const {cartProducts} = state.user;
+    return { cartProducts }
+}
+
+reviewProducts = connect(mapStateToProps)(reviewProducts);
+
+export default reviewProducts;
