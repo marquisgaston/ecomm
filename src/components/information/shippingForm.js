8e0e980daa2ab5c4e04d8818680912764bae5635
mjg1123 <53385037+mjg1123@users.noreply.github.com>
2019-12-13 11:04:47 -0500

    layed out shipping form feilds

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -9,55 +9,56 @@ import history from '../../history';
 class ShippingForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-
+  
         return (
-            <form onSubmit={handleSubmit} className={`${className} payment-form`}>
-                 <Field className='payment-form__name'
+            <form onSubmit={handleSubmit} className={`${className} shipping-form`}>
+                <Field className='shipping-form__name'
                 type='name'
                 title='Name'
                 placeholder='Name'
                 name='name'
                 component={FormInput}/>
-                <Field className='payment-form__address'
+                <Field className='shipping-form__address'
                 type='address'
                 title='Street Address'
                 placeholder='Street Address'
                 name='address'
                 component={FormInput}/>
-                <Field className='payment-form__city'
+                <Field className='shipping-form__city'
                 type='city'
                 title='City'
                 placeholder='City'
                 name='city'
                 component={FormInput}/>
-                <Field className='payment-form__state'
+                <Field className='shipping-form__state'
                 type='state'
                 title='State'
                 placeholder='State'
                 name='state'
                 component={FormInput}/>
-                <Field className='payment-form__zipcode'
+                <Field className='shipping-form__zipcode'
                 type='zipcode'
                 title='Zipcode'
                 placeholder='Zipcode'
                 name='zipcode'
                 component={FormInput}/>
-
-                <div className='payment-form__line'></div>
-                <Field className='payment-form__use-this-address'
-                onClick={() => history.push('/information-payment')}
+ 
+        
+                <div className='shipping-form__line'></div>
+                <Field className='shipping-form__use-this-address'
+                onClick={() => history.push('/information/payment')}
                 type='submit'
                 title='Use This Address'
                 name='use-this-address'
                 component={FormButton}/>
-                <div className='payment-form__line'></div>
-                <Field className='payment-form__back'
+                <Field className='shipping-form__back'
                 onClick={() => history.push('/signin')}
                 type='button'
                 title='Back'
                 name='back'
-                short = {true}
+                short={true}
                 component={FormButton}/>
+ 
             </form>
         )
     }
